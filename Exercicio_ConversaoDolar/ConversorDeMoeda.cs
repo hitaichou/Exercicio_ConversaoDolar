352e1a8557d2b36e8242d7d871720bf126cd2c60
hitaichou <holyfernandes@yahoo.co.uk>
2019-12-21 11:55:54 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio_ConversaoDolar/ConversorDeMoeda.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio_ConversaoDolar
+{
+    class ConversorDeMoeda
+    {
+        //constante taxa IOF
+        public static double iof = 0.06;
+
+        public static double ConversaoDolar (double cotD, double qtD)
+        {
+            return (qtD + (qtD * iof)) * cotD;
+        }
+    }
+}
---
+++ Exercicio_ConversaoDolar/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+namespace Exercicio_ConversaoDolar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com a cotação do Dolar: ");
+            double dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares você vai comprar? ");
+            double qtDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            //Chamada do método estático
+            double result = ConversorDeMoeda.ConversaoDolar(dolar, qtDolar);
+
+            Console.WriteLine("Valor a ser pago em reais = " + result.ToString("F2",CultureInfo.InvariantCulture));
+
+        }
+    }
+}
